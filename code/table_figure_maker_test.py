1a86beef44ebfda5d357c45080febddbdb6a7b14
mlinds <max.lindsay95@gmail.com>
2022-02-12 18:12:42 +0100

    working template now

--- code/table_figure_maker_test.py
+++ code/table_figure_maker_test.py
@@ -2,8 +2,8 @@
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(np.arange(1,101).reshape(10,10))
+df = pd.DataFrame(np.arange(1,101).reshape(5,20))
 df
-df.style.to_latex('../document/tables/testtable.tex',)
-df.plot(table=True).get_figure().savefig('../document/figures/testfig.png',Label="Test Table 1")
+df.style.to_latex('../document/tables/testtable.tex',label="TestTable1",caption='nice')
+df.plot(table=True).get_figure().savefig('../document/figures/testfig.png')
 # %%
