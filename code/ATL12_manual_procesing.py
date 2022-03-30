1f85393141b4fec110d7c2b34cd6317ce16387e8
Lindsay M.A.E. (Max) <Max.Lindsay@vanoord.com>
2022-03-30 15:46:57 +0200

    improved ATL data processing

--- code/ATL12_manual_procesing.py
+++ code/ATL12_manual_procesing.py
@@ -6,30 +6,20 @@ import numpy as np
 
 filepath = '../data/ATLAS_test_subset/177187253/processed_ATL12_20181014092931_02410101_003_01.nc'
 
-filelist = glob.glob('../data'+'/**/*.nc',recursive=True)
+filelist = glob.glob('../data/ATLAS_test_subset/*/*.nc',recursive=True)
 
 file = filelist[0]
 
 def read_atl13(file):
     ds = ncdf_ds(file)
-
+    
     beamgroups = [beam_name for beam_name in ds.groups if beam_name.startswith('gt')]
 
-    all_hvals = np.ma.masked_array()
-    all_lats = np.ma.masked_array()
-    all_lons = np.ma.masked_array()
 
-    for beam in beamgroups:
-        pass
-        # hvals = ds.groups[beam].groups['ssh_segments'].groups['heights'].variables['h'][:]
-        # np.append(all_hvals,hvals)
-        # lats = ds.groups[beam].groups['ssh_segments'].variables['latitude'][:]
-        # np.append(all_lats,lats)
-        # lons = ds.groups[beam].groups['ssh_segments'].variables['longitude'][:]
-        # np.append(all_lons,lons)
-        # hvals = ds.groups[beam].groups['ssh_segments'].groups['heights'].groups['h'][:]
+    # for beam in beamgroups:
+    #     grouppath = 'beam/ssh_segments/heights' 
+    #     array = xr.open_dataarray(heightgroup)
+    return beamgroups
 
-    return ds
-
-ds = read_atl13(file)
+print(read_atl13(filepath))
 # %%
