65574511581119dd825aa5fe6577eda5bc8478f3
mlinds <max.lindsay95@gmail.com>
2022-04-05 22:50:19 +0000

    changed requested variables again

--- code/variablelist.py
+++ code/variablelist.py
@@ -10,11 +10,15 @@ varpaths = [
     "heights/lon_ph",
     "heights/dist_ph_along",
     "heights/delta_time",
+    "heights/signal_conf_ph",
     "geophys_corr/dem_h",
     "geophys_corr/tide_ocean",
+    "geophys_corr/delta_time",
+    "geophys_corr/geoid",
+    "geophys_corr/geoid_free2mean"
 ]
 
-coverage_requested = ""
+coverage_requested = "/ancillary_data/start_rgt/,/ancillary_data/end_rgt/,/ancillary_data/data_start_utc/,/ancillary_data/data_end_utc/,/quality_assessment/qa_granule_pass_fail/,/quality_assessment/gt1l/,/quality_assessment/gt1r/,/quality_assessment/gt2l/,/quality_assessment/gt2r,/quality_assessment/gt3l/,/quality_assessment/gt3r"
 for beam in beamlist:
     for path in varpaths:
         coverage_requested = coverage_requested + beam + "/" + path + ","
