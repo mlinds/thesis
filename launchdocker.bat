4a3bc4817ad43cbe47745b106d6f31aabb5903df
mlinds <max.lindsay95@gmail.com>
2022-04-08 00:28:32 +0200

    removed root from docker launch file

--- launchdocker.bat
+++ launchdocker.bat
@@ -1,3 +1,3 @@
-docker run --rm -it --name thesis_container -e GRANT_SUDO=yes --user root --entrypoint /bin/bash -p 8888:8888 -v "C:\Users\maxli\OneDrive - Van Oord\Documents\thesis":/home/jovyan/work thesis-stack:latest
+docker run --rm -it --name thesis_container --entrypoint /bin/bash -p 8888:8888 -v "C:\Users\maxli\OneDrive - Van Oord\Documents\thesis":/home/jovyan/work thesis-stack:latest
 
 
