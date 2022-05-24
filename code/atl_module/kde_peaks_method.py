d052457e262bc10e3497fa668739105cbbfbe8e2
Max Lindsay <max.lindsay95@gmail.com>
2022-05-24 16:09:39 +0200

    add helper class to keep both kde outputs allows the rolling window to keep both values

--- code/atl_module/kde_peaks_method.py
+++ code/atl_module/kde_peaks_method.py
@@ -1,8 +1,12 @@
 import numpy as np
 from scipy.stats import gaussian_kde
 
+# This code is hard to read/maintain,maybe should change to try a numpy rolling window eventually
 
-def get_elev_at_max_density(point_array, threshold):
+
+# This function shouldn't be called directly, only from the AccumluateKDEs class so that both values can be stored
+def get_elev_at_max_density(point_array):
+
     # point_array = point_array[~np.isnan(point_array)]
     kde = gaussian_kde(point_array)
     kde_heights = kde.pdf(point_array)
@@ -11,6 +15,41 @@ def get_elev_at_max_density(point_array, threshold):
     z_at_kdemax = point_array[kde_heights.argmax()]
     # return if we don't have anything over the threshold
     # pd.DataFrame({'x':x,'y':kde_heights}).plot.scatter(x='x',y='y',xlim=[-35,0])
-    if max(kde_heights) <= threshold:
-        return np.NaN
     return z_at_kdemax,max_density
+
+
+
+# the above function is expensive to apply, so we can follow these instructions to avoid calling it twice:
+# https://stackoverflow.com/questions/22218438/returning-two-values-from-pandas-rolling-apply
+# class CountCalls:
+#     def __init__(self):
+#         self.counter = 0
+
+#     def your_function(self, window):
+#         retval = f(window)
+#         self.counter = self.counter + 1
+
+
+# TestCounter = CountCalls()
+
+# pandas.Series.rolling(your_seriesOrDataframeColumn, window = your_window_size).apply(TestCounter.your_function)
+
+# print TestCounter.counter
+
+
+class AccumulateKDEs:
+    def __init__(self):
+        self.index= 0
+        self.index_val_list = []
+        self.z_max_list = []
+        self.kde_val_list = []
+        self.returndict = {'z_kde':self.z_max_list,'kde_val':self.kde_val_list}
+
+    def your_function(self, zvals):
+        z_max_kde,kde_val = get_elev_at_max_density(zvals)
+        self.kde_val_list.append(kde_val)
+        self.z_max_list.append(z_max_kde)
+        self.index_val_list.append(self.index)
+        self.index = self.index+ 1
+        return z_max_kde
+
