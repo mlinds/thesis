fadf2d609223e47d6c480d780e78326d98c6bce6
Max Lindsay <max.lindsay95@gmail.com>
2022-05-24 14:39:08 +0200

    add setup file for pip import

---
+++ code/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+   name='atl_module',
+   version='1.0',
+   description='tools for bathymetric data assimilation',
+   author='Maxwell Lindsay',
+   packages=['atl_module']
+)
