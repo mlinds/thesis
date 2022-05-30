2b24123d0602fff38f5711745c117f393a436db8
Max Lindsay <max.lindsay95@gmail.com>
2022-05-30 15:15:35 +0200

    run linter

--- code/atl_module/refraction_correction.py
+++ code/atl_module/refraction_correction.py
@@ -2,7 +2,8 @@
 """
 import numpy as np
 
-def correct_refr(depth, pointing_vector_az,pointing_vector_elev):
+
+def correct_refr(depth, pointing_vector_az, pointing_vector_elev):
     """return the X,Y,Z corrections introduced by refraction, using parrish method
 
     Args:
@@ -12,26 +13,26 @@ def correct_refr(depth, pointing_vector_az,pointing_vector_elev):
 
     Returns:
         tuple: tuple of (Easting correcting, northing correction, Z correction)
-    """    
+    """
     refr_ind_air = 1.00029
     refr_ind_seawater = 1.34116
-    theta1 = 0.5*np.pi - pointing_vector_elev
-    angl_refr = np.arcsin((refr_ind_air*np.sin(theta1))/refr_ind_seawater)
+    theta1 = 0.5 * np.pi - pointing_vector_elev
+    angl_refr = np.arcsin((refr_ind_air * np.sin(theta1)) / refr_ind_seawater)
 
-    S = depth/np.cos(theta1)
-    R = S*(refr_ind_air/refr_ind_seawater)
+    S = depth / np.cos(theta1)
+    R = S * (refr_ind_air / refr_ind_seawater)
 
-    lambda_ = 0.5*np.pi - theta1
-    P = np.sqrt((R**2+S**2)-2*R*S*np.cos(theta1-angl_refr))
+    lambda_ = 0.5 * np.pi - theta1
+    P = np.sqrt((R**2 + S**2) - 2 * R * S * np.cos(theta1 - angl_refr))
     phi = theta1 - angl_refr
-    alpha = np.arcsin(R*np.sin(phi)/P)
+    alpha = np.arcsin(R * np.sin(phi) / P)
 
     beta = lambda_ - alpha
 
-    yshift = P*np.cos(beta)
-    zshift = P*np.sin(beta)
-    
-    eastingshift = yshift*np.sin(pointing_vector_az)
-    northingshift = yshift*np.cos(pointing_vector_az)
+    yshift = P * np.cos(beta)
+    zshift = P * np.sin(beta)
 
-    return eastingshift,northingshift,zshift
+    eastingshift = yshift * np.sin(pointing_vector_az)
+    northingshift = yshift * np.cos(pointing_vector_az)
+
+    return eastingshift, northingshift, zshift
