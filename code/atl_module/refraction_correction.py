d3a6f00f57b3bf390fc25134c071ebfcbe727b3a
Max Lindsay <max.lindsay95@gmail.com>
2022-05-30 11:40:41 +0200

    parrish method working

--- code/atl_module/refraction_correction.py
+++ code/atl_module/refraction_correction.py
@@ -2,14 +2,28 @@
 """
 import numpy as np
 
-def refr_corrected_depth(depth, pointing_vector_az):
-    theta2 = np.arcsin((n1/n2)*np.sin(theta1))
+def correct_refr(depth, pointing_vector_az,pointing_vector_elev):
+    """return the X,Y,Z corrections introduced by refraction, using parrish method
 
-    R = S(n1/n2)
+    Args:
+        depth (float): the current depth
+        pointing_vector_az (float): azimuth of pointing vector of the satellite in radians
+        pointing_vector_elev (float): Elevation of pointing vector of the satellite in radians
 
-    S = D/np.cos(theta1)
+    Returns:
+        tuple: tuple of (Easting correcting, northing correction, Z correction)
+    """    
+    refr_ind_air = 1.00029
+    refr_ind_seawater = 1.34116
+    theta1 = 0.5*np.pi - pointing_vector_elev
+    angl_refr = np.arcsin((refr_ind_air*np.sin(theta1))/refr_ind_seawater)
+
+    S = depth/np.cos(theta1)
+    R = S*(refr_ind_air/refr_ind_seawater)
+
     lambda_ = 0.5*np.pi - theta1
-    P = np.sqrt(R**2+S**2-2*R*S*cos(theta1-theta2))
+    P = np.sqrt((R**2+S**2)-2*R*S*np.cos(theta1-angl_refr))
+    phi = theta1 - angl_refr
     alpha = np.arcsin(R*np.sin(phi)/P)
 
     beta = lambda_ - alpha
@@ -20,4 +34,4 @@ def refr_corrected_depth(depth, pointing_vector_az):
     eastingshift = yshift*np.sin(pointing_vector_az)
     northingshift = yshift*np.cos(pointing_vector_az)
 
-    return depth
+    return eastingshift,northingshift,zshift
