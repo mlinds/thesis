e099c06fbe49c9af4b4f52a0b3d828821ae59c38
Max Lindsay <max.lindsay95@gmail.com>
2022-06-09 17:37:08 +0200

    Kalman and kriging both sort of working

--- code/setup.py
+++ code/setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
+
 setup(
-   name='atl_module',
-   version='1.0',
-   description='tools for bathymetric data assimilation',
-   author='Maxwell Lindsay',
-   packages=['atl_module']
+    name="atl_module",
+    version="1.0",
+    description="tools for bathymetric data assimilation",
+    author="Maxwell Lindsay",
+    packages=["atl_module"],
 )
