82b1f9854d4a3d6b8b95c074af87c0fe8bdc6a3f
Max Lindsay <max.lindsay95@gmail.com>
2022-06-27 19:32:15 +0200

    commit final version before removal

--- code/qgis-scripts/EE_Mangroves.py
+++ code/qgis-scripts/EE_Mangroves.py
@@ -1,7 +1,8 @@
 import ee
 from ee_plugin import Map
+
 ee.Initialize()
 
-image = ee.ImageCollection('LANDSAT/MANGROVE_FORESTS')
-    
-Map.addLayer(image, {'min': 0, 'max': 1}, 'Mangroves', True)
+image = ee.ImageCollection("LANDSAT/MANGROVE_FORESTS")
+
+Map.addLayer(image, {"min": 0, "max": 1}, "Mangroves", True)
