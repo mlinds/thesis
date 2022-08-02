10a18a62aa50a2fc6c36bc8c539492e7f58b41a1
Max Lindsay <max.lindsay95@gmail.com>
2022-08-02 17:45:51 +0200

    start adding plotting module

---
+++ code/atl_module/plotting.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def error_lidar_pt_vs_truth_pt(df_in,site_name):
+    ax = df_in.plot.scatter(
+        x="true_elevation",
+        y="z_kde",
+        xlabel="True Elevation [m +MSL]",
+        ylabel="Calculated Elevation [m +MSL]",
+        title=f'Lidar Point Vs. Truth Point: {site_name}',
+        figsize=(5,5),
+        alpha=0.3
+    )
+
+    one_to_one_ln_st = min(df_in.true_elevation.min(), df_in.z_kde.min())
+    one_to_one_ln_end = max(df_in.true_elevation.max(), df_in.z_kde.max())
+    ax.plot(
+        (one_to_one_ln_st, one_to_one_ln_end),
+        (one_to_one_ln_st, one_to_one_ln_end),
+        c="red",
+        label='1 = 1'
+    )
+    ax.legend()
+    return ax
+    
