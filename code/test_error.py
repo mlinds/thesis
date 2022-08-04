f6de275f4c729b7c803b86b22a6ba6984abbf51f
Max Lindsay <max.lindsay95@gmail.com>
2022-08-04 11:22:36 +0200

    formatting

--- code/test_error.py
+++ code/test_error.py
@@ -1,38 +1,44 @@
 # %%
-from atl_module import icesat_bathymetry
-from atl_module.raster_interaction import query_raster
-from atl_module.geospatial_functions import (
+from atl_module.bathymetry_extraction import icesat_bathymetry
+from atl_module.geospatial_utils.raster_interaction import query_raster
+from atl_module.geospatial_utils.geospatial_functions import (
     to_refr_corrected_gdf,
 )
 import pandas as pd
-from atl_module.error_calc import calc_rms_error
+from atl_module.error_calc import icesat_error_rms_mae
 from glob import iglob
 from sklearn.metrics import mean_absolute_error
+import geopandas as gpd
 
 # %%
-folderpath = "../data/test_sites/Martinique"
+folderpath = "../data/test_sites/florida_keys"
 resultdict = {}
 
 resultdict["Threshold value"] = []
 resultdict["RMS Error"] = []
 # resultdict['MAE Error'] = []
 
-for windowval in [100, 200]:
+for windowval in [150]:
     # get_bathymetry
     bathy_points = icesat_bathymetry.bathy_from_all_tracks_parallel(
-        folderpath, window=windowval, threshold_val=0.0, req_perc_hconf=60
+        folderpath,
+        window=windowval,
+        threshold_val=0.0,
+        req_perc_hconf=40,
+        window_meters=None,
+        min_photons=None,
     )
 
     true_bathy = query_raster(
         bathy_points,
-        src="../data/test_sites/Martinique/in-situ-dem/DONNEES/MNT_ANTS100m_HOMONIM_WGS84_PBMA_ZNEG.asc",
+        src="../data/test_sites/florida_keys/in-situ-DEM/2019_irma.vrt",
     )
 
     bathy_points = bathy_points.assign(fema_elev=true_bathy)
     bathy_points = bathy_points[bathy_points.fema_elev != -9999.0]
 
     bathy_points.eval("error = z_kde-fema_elev", inplace=True)
-    rms = calc_rms_error(bathy_points, ["fema_elev"])
+    rms = icesat_error_rms_mae(bathy_points, ["fema_elev"])
     # mae = mean_absolute_error(bathy_points.fema_elev,bathy_points.z_kde)
 
     resultdict["Threshold value"].append(windowval)
@@ -52,3 +58,26 @@ bathy_points.plot.scatter(
     y="z_kde", x="fema_elev", figsize=(20, 20), c="kde_val", cmap="viridis"
 )
 # %%
+join1 = (
+    bathy_points.assign(date=bathy_points.delta_time.dt.date)
+    .astype({"date": "string"})
+    .set_index(["date", "beam"])
+)
+join2 = (
+    gpd.read_file("../data/global_analysis/color_points_mean.gpkg")
+    .rename({"beamtype": "beam"}, axis=1)
+    .set_index(["date", "beam"])
+    .zsd
+)
+merged = pd.merge(join1, join2, right_index=True, left_index=True, how="left")
+
+error_df = (
+    merged.eval("error=error**2")
+    .groupby(["date", "beam"])
+    .mean()
+    .eval("error=error**(0.5)")
+)
+
+error_df.plot.scatter(y="error", x="zsd")
+error_df.plot.scatter(y="error", x="oc_hconf_perc")
+error_df.plot.scatter(y="error", x="kde_val")
