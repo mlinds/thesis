3298391bf19d594bcf5ace56a7b34bb868e081a2
Max Lindsay <max.lindsay95@gmail.com>
2022-08-05 14:53:46 +0200

    add tidal correction

--- code/atl_module/bathymetry_extraction/point_dataframe_filters.py
+++ code/atl_module/bathymetry_extraction/point_dataframe_filters.py
@@ -9,6 +9,11 @@ import numpy as np
 p = Path(__file__).parents[3]
 
 
+def add_msl_corrected_seafloor_elev(df_in):
+    dac_tide = df_in.z_kde - df_in.dac_corr + df_in.tide_ocean_corr
+    return df_in.assign(sf_elev_MSL=dac_tide)
+
+
 def filter_high_returns(df, max_geoid_high_z):
     """Remove returns above *level* which is an elevation in meters
 
