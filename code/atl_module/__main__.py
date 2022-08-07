45ac393e556a34b76a22b00a12d3596854a85f62
Max Lindsay <max.lindsay95@gmail.com>
2022-08-07 15:20:50 +0200

    temp change for oahu

--- code/atl_module/__main__.py
+++ code/atl_module/__main__.py
@@ -23,30 +23,30 @@ if args.verbose:
 
 site = GebcoUpscaler(
     f"{args.sitename}",
-    f"../data/test_sites/{args.sitename}/in-situ-DEM/truth.vrt",
+    f"../data/test_sites/oahu/in-situ-DEM/truth.vrt",
 )
 # site.download_ATL03()
 # site.recalc_tracklines_gdf()
 # site.subset_gebco(hres=50)
-# site.find_bathy_from_icesat(
-#     window=200,
-#     threshold_val=0.0,
-#     req_perc_hconf=60,
-#     window_meters=None,
-#     min_photons=None,
-#     min_kde=0.0,
-#     low_limit=-40,
-#     high_limit=2,
-#     rolling_window=200,
-#     max_sea_surf_elev=1,
-#     filter_below_z=-80,
-#     filter_below_depth=-80,
-#     n=1,
-#     max_geoid_high_z=5,
-# )
-# site.subset_gebco(hres=50)
-# print(site.lidar_error())
-# site.plot_lidar_error()
+site.find_bathy_from_icesat(
+    window=200,
+    threshold_val=0.0,
+    req_perc_hconf=60,
+    window_meters=None,
+    min_photons=None,
+    min_kde=0.0,
+    low_limit=-40,
+    high_limit=2,
+    rolling_window=200,
+    max_sea_surf_elev=1,
+    filter_below_z=-80,
+    filter_below_depth=-80,
+    n=1,
+    max_geoid_high_z=5,
+)
+site.subset_gebco(hres=50)
+print(site.lidar_error())
+site.plot_lidar_error()
 site.kriging(npts=1800, samplemethod="dart", kr_model="uk")
 site.kalman_update(0.75)
 print(site.raster_rmse(error_out=True))
