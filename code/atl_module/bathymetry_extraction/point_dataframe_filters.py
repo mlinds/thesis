b7686b9bfa452dbb4f10a8b6064bcd2306b06282
Max Lindsay <max.lindsay95@gmail.com>
2022-08-08 10:09:50 +0200

    switch to additive tide correction

--- code/atl_module/bathymetry_extraction/point_dataframe_filters.py
+++ code/atl_module/bathymetry_extraction/point_dataframe_filters.py
@@ -11,7 +11,7 @@ p = Path(__file__).parents[3]
 
 
 def add_msl_corrected_seafloor_elev(df_in):
-    dac_tide = df_in.z_kde  # - df_in.dac_corr - df_in.tide_ocean_corr
+    dac_tide = df_in.z_kde + df_in.dac_corr + df_in.tide_ocean_corr
     return df_in.assign(sf_elev_MSL=dac_tide)
 
 
