19b537e33ada205328bc7ffa2d9712acecac2026
Max Lindsay <max.lindsay95@gmail.com>
2022-08-08 10:15:30 +0200

    commenting out some blocks

--- code/atl_module/__main__.py
+++ code/atl_module/__main__.py
@@ -1,7 +1,8 @@
-from atl_module.core import GebcoUpscaler
 import argparse
+
 import logzero
-from logzero import setup_logger, DEBUG
+from atl_module.core import GebcoUpscaler
+from logzero import DEBUG, setup_logger
 
 # runlogger = setup_logger(name="mainrunlogger", logfile="./run_log.log")
 parser = argparse.ArgumentParser()
@@ -27,26 +28,27 @@ site = GebcoUpscaler(
 )
 # site.download_ATL03()
 # site.recalc_tracklines_gdf()
+# site.find_bathy_from_icesat(
+#     window=200,
+#     threshold_val=0.0,
+#     req_perc_hconf=0,
+#     window_meters=None,
+#     min_photons=None,
+#     min_kde=0.1,
+#     low_limit=-40,
+#     high_limit=2,
+#     rolling_window=200,
+#     max_sea_surf_elev=1,
+#     filter_below_z=-40,
+#     filter_below_depth=-40,
+#     min_ph_count=35,
+#     n=1,
+#     max_geoid_high_z=5,
+# )
 # site.subset_gebco(hres=50)
-site.find_bathy_from_icesat(
-    window=200,
-    threshold_val=0.0,
-    req_perc_hconf=60,
-    window_meters=None,
-    min_photons=None,
-    min_kde=0.0,
-    low_limit=-40,
-    high_limit=2,
-    rolling_window=200,
-    max_sea_surf_elev=1,
-    filter_below_z=-80,
-    filter_below_depth=-80,
-    n=1,
-    max_geoid_high_z=5,
-)
-site.subset_gebco(hres=50)
-print(site.lidar_error())
-site.plot_lidar_error()
-site.kriging(npts=1800, samplemethod="dart", kr_model="uk")
-site.kalman_update(0.75)
-print(site.raster_rmse(error_out=True))
+# print(site.lidar_error())
+# site.plot_lidar_error()
+# site.kriging(npts=1800, samplemethod="dart", kr_model="uk")
+site.kalman_update(1)
+print(site.raster_rmse())
+site.run_summary()
