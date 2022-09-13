65d43ea4f042124a827efba68a3f9c36ca1279f1
Max Lindsay <max.lindsay95@gmail.com>
2022-09-13 20:47:55 +0200

    add truth raster plot function

--- code/atl_module/plotting.py
+++ code/atl_module/plotting.py
@@ -1,7 +1,10 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+import rasterio
+from rasterio.plot import show as rastershow
 
 
-def error_lidar_pt_vs_truth_pt(df_in: pd.DataFrame, site_name):
+def error_lidar_pt_vs_truth_pt(df_in: pd.DataFrame, site_name, error_dict):
     ax = df_in.plot.scatter(
         x="true_elevation",
         y="z_kde",
@@ -20,5 +23,32 @@ def error_lidar_pt_vs_truth_pt(df_in: pd.DataFrame, site_name):
         c="red",
         label="1 = 1",
     )
-    ax.legend()
+    ax.text(0.1, 0.9, s=f'$RMSE = {error_dict["RMSE"]:.2f}m$', transform=ax.transAxes)
+    ax.text(0.1, 0.8, s=f'$R^2 = {error_dict["R2 Score"]:.2f}$', transform=ax.transAxes)
+    # ax.text(0.2,0.1,f'RMSE = {error_dict['RMSE']}')
+    # ax.text('MAE')
+    # ax.legend()
+
     return ax
+
+
+def map_ground_truth_data(truthdata_path, plottitle):
+    # TODO add basemap, currently causing a bug
+    with rasterio.open(truthdata_path) as truthraster:
+        fig, ax = plt.subplots(figsize=(20, 17))
+        ax.set_xlabel(f"Degrees longitude in {truthraster.crs}")
+        ax.set_ylabel(f"Degrees latitude in {truthraster.crs}")
+        ax.set_title(plottitle)
+        image_hidden = ax.imshow(
+            truthraster.read(1, masked=True),
+            # contour=True,
+            cmap="inferno",
+        )
+        rastershow(
+            truthraster,
+            cmap="inferno",
+            ax=ax,
+        )
+
+        fig.colorbar(image_hidden, ax=ax)
+        return fig
