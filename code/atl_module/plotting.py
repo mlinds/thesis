6f2b63283d3ff4c90162393b0db5528a64797f6d
Max Lindsay <max.lindsay95@gmail.com>
2022-09-18 11:28:09 +0200

    add code to plot the kde, kdeval, and binstats

--- code/atl_module/plotting.py
+++ code/atl_module/plotting.py
@@ -1,5 +1,6 @@
 import contextily as cx
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import rasterio
 from rasterio.plot import show as rastershow
@@ -78,3 +79,45 @@ def plot_tracklines_overview(tracklines_gdf):
     ax.set_ylabel(f"Northing in {tracklines_gdf.crs.name}")
     ax.set_title("Study site and tracklines")
     return ax
+
+
+def plot_transect_results(subsurfacedf, bathy_df, figpath):
+    # this could be two or 4 functions :(
+    fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(20, 10))
+    ax = axes[0]
+    ax.plot(subsurfacedf.delta_time, subsurfacedf.sea_level_interp, label="sea surf")
+    ax.plot(subsurfacedf.delta_time, subsurfacedf.sea_level_interp - 1, label="sea surf - 1m")
+    ax.scatter(x=subsurfacedf.delta_time, y=subsurfacedf.Z_refr, s=4)
+    ax.plot(bathy_df.delta_time, bathy_df.z_kde, c="red", alpha=0.7)
+    ax.plot(bathy_df.delta_time, bathy_df.true_elevation, c="black", alpha=0.7)
+    ax.legend()
+    # add the sea surface
+    # ax.axhline(bathy_df.sea_level_interp.iloc[2],c='green')
+
+    # add another axis for the kde
+    ax2 = ax.twinx()
+    ax2.plot(bathy_df.delta_time, bathy_df.kde_val, c="green")
+    ax2.axhline(bathy_df.kde_val.mean(), label="mean kde", c="purple")
+    ax2.axhline(bathy_df.kde_val.median(), label="median kde", c="red")
+    # ax2.plot(bathy_df.delta_time,bathy_df.ph_count,label='ph count')
+    ax2.legend(loc="lower right")
+
+    bathy_df = bathy_df.assign(error=bathy_df.true_elevation - bathy_df.z_kde)
+    bathy_df = bathy_df.assign(sqerror=bathy_df.error**2)
+
+    time_secs = bathy_df.delta_time.max() - bathy_df.delta_time.min()
+
+    nbins = max(1, int(time_secs.value / 4000000))
+    bindf = bathy_df.groupby(pd.cut(bathy_df.delta_time, nbins)).agg(
+        {"X": "count", "sqerror": lambda x: np.power(np.mean(x), 0.5), "kde_val": "mean"}
+    )
+    # set ax to be the second subplot
+    ax = axes[1]
+    # plot the error by bin onto the second axis
+    bindf.plot.scatter(y="sqerror", x="kde_val", c="X", cmap="viridis", ax=ax)
+    ax.axvline(bindf.kde_val.median(), label="median kde bins")
+    ax.axvline(bindf.kde_val.mean(), label="mean kde bins", c="red")
+    ax.legend()
+    ax.set_title(f"binning with {nbins}")
+
+    fig.savefig(figpath, bbox_inches="tight")
