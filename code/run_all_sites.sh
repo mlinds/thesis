efe82169b4ca98016b841f557dc6b24fa9df18cf
Max Lindsay <max.lindsay95@gmail.com>
2022-09-18 22:23:56 +0200

    reran all kriging

--- code/run_all_sites.sh
+++ code/run_all_sites.sh
@@ -1,5 +1,7 @@
-for d in ../data/test_sites/*/; 
+# for d in ../data/test_sites/*/; 
+for d in oahu7 oahu8 stcroix; 
 do
 echo "starting $(basename $d)"
-python -m atl_module "$(basename $d)" -tr -b -lrmse
+python -m atl_module "$(basename $d)" -lrmse -g 50 -kr -ka 1.5 -rmse
+# python -m atl_module "$(basename $d)" -b -lrmse
 done
