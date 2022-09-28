4f3f8745a7023f0f9d77fd5444d35eb790a7ac3f
Max Lindsay <max.lindsay95@gmail.com>
2022-09-28 15:35:00 +0200

    remove old code

--- code/atl_module/ocean_color.py
+++ code/atl_module/ocean_color.py
@@ -7,14 +7,11 @@ import requests
 import xarray as xr
 from atl_module.secret_vars import COPERNICUS_PW, COPERNICUS_USERNAME
 
-# from pandas import Int16Dtype, MultiIndex
-
 DAP_URL = "https://my.cmems-du.eu/thredds/dodsC/cmems_obs-oc_glo_bgc-transp_my_l4-gapfree-multi-4km_P1D"
 
+
 # setup the session upon import
-def _setup_globcolor_api_session(
-    COPERNICUS_USERNAME: str, COPERNICUS_PW: str, DAP_URL: str
-):
+def _setup_globcolor_api_session(COPERNICUS_USERNAME: str, COPERNICUS_PW: str, DAP_URL: str):
     """Create a dataset of the GlobColour Data using the correct login. The xarray dataset uses lazy loading so it can be generated then values can be looked up as needed.
 
     Args:
@@ -33,8 +30,11 @@ def _setup_globcolor_api_session(
     return xr.open_dataset(store)
 
 
+ds = _setup_globcolor_api_session(COPERNICUS_USERNAME, COPERNICUS_PW, DAP_URL)
+
+
 def get_zsd_info(lat: float, lon: float, dates: str or datetime) -> tuple:
-    ds = _setup_globcolor_api_session(COPERNICUS_USERNAME, COPERNICUS_PW, DAP_URL)
+
     """lookup the disk depth info at a certain location and time.
 
     Args:
@@ -59,52 +59,14 @@ def get_zsd_info(lat: float, lon: float, dates: str or datetime) -> tuple:
     # get an array of depth and uncertainty
     secchi_depth_array = subset.ZSD.to_numpy()
     secchi_depth_uncertainty_array = subset.ZSD_uncertainty.to_numpy()
-    return secchi_depth_array, secchi_depth_uncertainty_array
-    # return secchi_depth_array
-
-
-# def get_color_dataframe(lat, lon, dates,ds):
-#     # select the latitude and longitude
-#     subset = ds.sel(
-#         lat=lat,
-#         lon=lon,
-#         method="nearest"
-#         # get the closest date
-#     ).sel(time=dates, method="nearest")
-#     return subset.to_dataframe()
-
-
-def add_secchi_depth_to_tracklines(df_input: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    """Accepts a dataframe of tracklines and returns the same dataframe with a new column for the Secchi Disk Depth and the uncertainty
-
-    Args:
-        df_input (gpd.GeoDataFrame): Geodataframe of the tracklines
-
-    Returns:
-        gpd.GeoDataFrame: Same geodataframe with 2 new columns
-    """
-    # create a temporary reprojected version of the dataframe
-
-    temp = df_input.assign(
-        xcoord=df_input.to_crs("EPSG:4326").centroid.x,
-        ycoord=df_input.to_crs("EPSG:4326").centroid.y,
-        date=pd.to_datetime(df_input.date).dt.date,
+    diffuse_attenuation_array = subset.KD490.to_numpy()
+    diffuse_attenuation_unc_array = subset.KD490_uncertainty
+    return (
+        secchi_depth_array,
+        secchi_depth_uncertainty_array,
+        diffuse_attenuation_array,
+        diffuse_attenuation_unc_array,
     )
-    # get a series of the depth and uncertainty values
-    zsd_vals, zsd_sigma_vals = get_zsd_info(
-        temp.ycoord.values, temp.xcoord.values, temp.date.values
-    )
-    # add the series to the original dataframe and return
-    return df_input.assign(
-        secchi_depth=zsd_vals,
-        secchi_depth_unc=zsd_sigma_vals,
-    )
-
-
-# def add_secchi_depth_to_photons(df_input: gpd.GeoDataFrame):
-#     rounded_df = df_input.loc[:, ["X", "Y"]].apply(lambda x: round(x * 16) / 16)
-#     rounded_df["date"] = pd.to_datetime(df_input.delta_time).dt.date
-#     rounded_df.drop_duplicates(inplace=True)
 
 
 def _resample_line(linestring):
@@ -116,22 +78,32 @@ def _resample_line(linestring):
 
 
 def create_zsd_points_from_tracklines(df_input):
+
     # change the datetime to only the nearest day
     df_input["date"] = pd.to_datetime(df_input.date).dt.date
     # empty list to keep the points and their dates
     allpts = []
+    # loop over each trackline
     for trackline in df_input.itertuples():
         points = _resample_line(trackline.geometry)
-        date_points = [[trackline.date, pointgeom] for pointgeom in points]
+        # print(trackline)
+        date_points = [[trackline.date, pointgeom, trackline.beam] for pointgeom in points]
         allpts = allpts + date_points
     sample_pts_gdf = gpd.GeoDataFrame(
-        allpts, geometry="geometry", columns=["date", "geometry"], crs="EPSG:4326"
+        allpts,
+        geometry="geometry",
+        columns=["date", "geometry", "beam"],
+        crs="EPSG:4326",
     )
     # get the actualy secchi depth info
-    a, b = get_zsd_info(
+    a, b, c, d = get_zsd_info(
         sample_pts_gdf.geometry.y, sample_pts_gdf.geometry.x, sample_pts_gdf.date
     )
     sample_pts_gdf = sample_pts_gdf.assign(
-        zsd=a, sigma_zsd=b, date=pd.to_datetime(sample_pts_gdf.date)
+        zsd=a,
+        sigma_zsd=b,
+        diff_atten=c,
+        diff_atten_unc=d,
+        date=pd.to_datetime(sample_pts_gdf.date),
     )
     return sample_pts_gdf
